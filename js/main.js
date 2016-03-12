661a071c9e134eb80a580ec56f8320d64d542ce8
Luis Silva <lotusms@outlook.com>
2016-03-12 09:28:26 -0500

    Minified

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 
 var app = angular.module('angula', ['ngRoute', 'ui.bootstrap', 'ngAnimate', 'ngMaterial', 'uiGmapgoogle-maps', 'rev.slider']);
 
+
+
+
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     // Home
@@ -532,62 +535,62 @@ app.controller("dataImagesWork", function ($scope) {
     $scope.images_work = [
           {
               "name": 'PA Liquor Control Board',
-              "src": "Liquor-Control-Board.png",
+              "src": "Liquor-Control-Board340.png",
               "link": "http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/StoreCatalogDisplay?storeId=10051&catalogId=10051&langId=-1"
           },
           {
               "name": 'PA Provider Self Service',
-              "src": "Provider-Services-of-PA.png",
+              "src": "Provider-Services-of-PA340.png",
               "link": 'https://www.pelican.state.pa.us/provider/default.aspx?TYPE=33554433&REALMOID=06-ccc2a1cb-0683-440f-bfa6-cad042af12ba&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-ZW9pIAMntaN%2bR%2fAG5q4UUzXVDz%2bKoMG2pXXJCs1re8tkRKCZKtgqJypoW9Af5ilo&TARGET=-SM-https%3a%2f%2fwww%2epelican%2estate%2epa%2eus%2fprovider%2fui%2fhome%2easpx#'
           },
           {
               "name": 'Job Gateway',
-              "src": "JobGateway.png",
+              "src": "JobGateway340.png",
               "link": 'https://www.jobgateway.pa.gov/jponline/Admin/Common/Portal.aspx?w@cIRFbHc_kyIfEUDPqYpUspb3u_@dnsvVOXen8isTEcdYg4grazpGUr2rtw4QN@odjSSGi8RwBm4_nraX__ITaRZJT8HYWbn3TgGPFcA0A-6xUQEkG_y@pHEMFmjDEEwSppJHEDfIGrVG6yQ4mCf4aazFA4QpEE'
           },
           {
               "name": 'Keystone Canine Rescue',
-              "src": "KeystoneCanineRescue.png",
+              "src": "KeystoneCanineRescue340.png",
               "link": 'http://www.keystonecaninerescue.org/'
           },
           {
               "name": 'Thrifty Elegance',
-              "src": "ThriftyElegance.png",
+              "src": "ThriftyElegance340.png",
               "link": 'http://www.thriftyelegancepa.com/'
           },
           {
               "name": 'Team Balance Harrisburg',
-              "src": "Team-Balance-Harrisburg.png",
+              "src": "Team-Balance-Harrisburg340.png",
               "link": 'http://www.teambalanceharrisburg.com/'
           },
           {
               "name": 'Witmers Feed & Grain',
-              "src": "Witmers.png",
+              "src": "Witmers340.png",
               "link": 'http://www.witmersfeed.com/'
           },
           {
               "name": 'R & J Dairy Consulting',
-              "src": "randjdairy.png",
+              "src": "randjdairy340.png",
               "link": 'http://www.randjdairy.com/'
           },
           {
               "name": 'Modern Vintage 1005',
-              "src": "MV1005.png",
+              "src": "MV1005340.png",
               "link": 'http://www.modernvintage1005.com/'
           },
           {
               "name": 'Feed Commodities',
-              "src": "FeedCommodities.png",
+              "src": "FeedCommodities340.png",
               "link": 'http://www.feedcommodities.com/'
           },
           {
               "name": 'VisionSpec Home',
-              "src": "VisionSpecHome.png",
+              "src": "VisionSpecHome340.png",
               "link": 'http://www.visionspechome.com/'
           },
           {
               "name": 'Nick & Sons Contracting',
-              "src": "Nick_Sons.png",
+              "src": "Nick_Sons340.png",
               "link": 'http://www.nickandsonsllc.com/'
           }];
 
